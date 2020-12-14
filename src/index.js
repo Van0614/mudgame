6e58302c46aa970e46a744bc6ac5b6caff2f77a3
unknown <vanessa614@snu.ac.kr>
2020-12-14 16:42:31 +0900

    1214 16:40 수정본

--- src/index.js
+++ src/index.js
@@ -2,27 +2,28 @@ const express = require("express");
 const fs = require("fs");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
+const { encryptPassword } = require("./util.js");
 
-const {constantManager, mapManager} = require("./datas/Manager");
-const {Player} = require("./models/Player");
+const { constantManager, mapManager } = require("./datas/Manager");
+const { Player } = require("./models/Player");
 
 const app = express();
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
 mongoose.connect(
-  "mongodb+srv://tester:Z5knBqgfuOqzb2Pu@cluster0.ye4cg.mongodb.net/Game0?retryWrites=true&w=majority",
-  {useNewUrlParser: true, useUnifiedTopology: true}
+  "mongodb+srv://vanessa:actjhj614^^@cluster0.gtu8i.mongodb.net/games?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 const authentication = async (req, res, next) => {
-  const {authorization} = req.headers;
+  const { authorization } = req.headers;
   if (!authorization) return res.sendStatus(401);
   const [bearer, key] = authorization.split(" ");
   if (bearer !== "Bearer") return res.sendStatus(401);
-  const player = await Player.findOne({key});
+  const player = await Player.findOne({ key });
   if (!player) return res.sendStatus(401);
 
   req.player = player;
@@ -30,7 +31,7 @@ const authentication = async (req, res, next) => {
 };
 
 app.get("/", (req, res) => {
-  res.render("index", {gameName: constantManager.gameName});
+  res.render("index", { gameName: constantManager.gameName });
 });
 
 app.get("/game", (req, res) => {
@@ -38,14 +39,18 @@ app.get("/game", (req, res) => {
 });
 
 app.post("/signup", async (req, res) => {
-  const {id, password, name,} = req.body;
+  const { id, password, name } = req.body;
+  const encryptedPassword = encryptPassword(password);
 
-  if (await Player.exists({id})) { // 새롭게 계정을 만드는 경우.
-    return res.status(400).send({error: "Player with same id already exists"});
+  if (await Player.exists({ id })) {
+    // 새롭게 계정을 만드는 경우.
+    return res
+      .status(400)
+      .send({ error: "Player with same id already exists" });
   } else {
     const player = new Player({
       id,
-      password,
+      password: encryptedPassword,
       name,
       maxHP: 10,
       HP: 10,
@@ -60,53 +65,67 @@ app.post("/signup", async (req, res) => {
       level: 1,
       statChangeChance: 5
     });
-    await player.save()
-    return res.status(200).send({success: true, message: "Successfully made new user. Please log in."});
+    await player.save();
+    return res.status(200).send({
+      success: true,
+      message: "Successfully made new user. Please log in."
+    });
   }
-})
+});
 
 app.post("/stat", async (req, res) => {
   // Change starting stat, deduct change by 1
-  const {id, password, str, def, hp, final} = req.body;
-  const player = await Player.findOne({id, password});
-  if (player.statChangeChance > 0 || final === 'true') {
+  const { id, password, str, def, maxHP, final } = req.body;
+  const player = await Player.findOne({
+    id,
+    password: encryptPassword(password)
+  });
+  if (player.statChangeChance > 0 || final === "true") {
     player.str = str;
     player.def = def;
-    player.hp = hp;
+    player.MaxHp = MaxHP;
+    player.Hp = MaxHP;
     player.statChangeChance -= 1;
     if (final === true || final === "true") {
-      player.statChangeChance = 0
+      player.statChangeChance = 0;
     }
     await player.save();
     return res.send({
       success: true,
-      message: 'changed starting stat',
+      message: "changed starting stat",
       chance: player.statChangeChance,
       str: str,
       def: def,
-      hp: hp,
+      MaxHP: MaxHP
     });
   } else {
-    return res.status(400).send({error: "Chance all used up. Cannot change stat"});
+    return res
+      .status(400)
+      .send({ error: "Chance all used up. Cannot change stat" });
   }
-})
+});
 
 app.post("/signin", async (req, res) => {
-  const {id, password} = req.body;
-  if (await Player.exists({id, password})) {
+  const { id, password } = req.body;
+  if (await Player.exists({ id, password: encryptPassword(password) })) {
     // TODO: auth 체크
     const key = crypto.randomBytes(24).toString("hex");
-    const player = await Player.findOne({id, password})
+    const player = await Player.findOne({
+      id,
+      password: encryptPassword(password)
+    });
     player.key = key;
     await player.save();
-    return res.send({key});
+    return res.send({ key });
   } else {
-    return res.status(400).send({error: "No such player. Wrong id or password"});
+    return res
+      .status(400)
+      .send({ error: "No such player. Wrong id or password" });
   }
 });
 
 app.post("/action", authentication, async (req, res) => {
-  const {action} = req.body;
+  const { action } = req.body;
   const player = req.player;
   let event = null;
   let field = null;
@@ -138,15 +157,16 @@ app.post("/action", authentication, async (req, res) => {
 
     // 이벤트를 1) 고르고, 2) 실행한다.
     // 전투시, damageCalculator(공격력, 수비력)=> {감소하는 체력} 함수 활용
-    if (events.length > 0) {  // TODO : 이 부분 통째로 변경
+    if (events.length > 0) {
+      // TODO : 이 부분 통째로 변경
       const _event = events[0];
       if (_event.type === "battle") {
         // TODO: 이벤트 별로 events.json 에서 불러와 이벤트 처리
 
-        event = {description: "늑대와 마주쳐 싸움을 벌였다."};
+        event = { description: "늑대와 마주쳐 싸움을 벌였다." };
         player.incrementHP(-1);
       } else if (_event.type === "item") {
-        event = {description: "포션을 획득해 체력을 회복했다."};
+        event = { description: "포션을 획득해 체력을 회복했다." };
         player.incrementHP(1);
         player.HP = Math.min(player.maxHP, player.HP + 1);
       }
@@ -155,9 +175,9 @@ app.post("/action", authentication, async (req, res) => {
     // 사망 이벤트 처리
     if (player.HP <= 0) {
       // TODO: 으악 주금!
-      player.HP = player.maxHP
-      player.x = 0
-      player.y = 0
+      player.HP = player.maxHP;
+      player.x = 0;
+      player.y = 0;
       // TODO: 사망시 랜덤하게 아이템을 잃어버린다.
     }
 
@@ -169,11 +189,11 @@ app.post("/action", authentication, async (req, res) => {
       actions.push({
         url: "/action",
         text: i,
-        params: {direction: i, action: "move"}
+        params: { direction: i, action: "move" }
       });
   });
 
-  return res.send({player, field, event, actions});
+  return res.send({ player, field, event, actions });
 });
 
 app.listen(3000);
--- src/models/Player.js
+++ src/models/Player.js
@@ -1,35 +1,47 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
+const playerSchema = new Schema({
   id: String,
   password: String,
+  key: String,
 
   name: String,
-
   level: Number,
   exp: Number,
 
-  maxHP: {type: Number, default: 10},
-  HP: {type: Number, default: 10},
-  str: {type: Number, default: 5},
-  def: {type: Number, default: 5},
-  itemStr: {type: Number, default: 5},   // TODO: 아이템 능력치를 여기에 추가
-  itemDef: {type: Number, default: 5},   // TODO: 아이템 능력치를 여기에 추가
-  x: {type: Number, default: 0},
-  y: {type: Number, default: 0},
-  items: {type: Array, default: []},   // TODO: 아이템들을 여기에 추가
-  exp: {type: Number, default: 0},
-  level: {type: Number, default: 1},
+  maxHP: { type: Number, default: 10 },
+  HP: { type: Number, default: 10 },
+  str: { type: Number, default: 5 },
+  def: { type: Number, default: 5 },
+  itemStr: { type: Number, default: 0 }, // TODO: 아이템 능력치를 여기에 추가
+  itemDef: { type: Number, default: 0 }, // TODO: 아이템 능력치를 여기에 추가
+  x: { type: Number, default: 0 },
+  y: { type: Number, default: 0 },
+  items: { type: Array, default: [] }, // TODO: 아이템들을 여기에 추가
+  exp: { type: Number, default: 0 },
+  maxExp: { type: Number, default: 10 },
+  level: { type: Number, default: 1 },
 
-  statChangeChance: {type: Number, default: 1},
+  statChangeChance: { type: Number, default: 1 }
 });
-schema.methods.incrementHP = function (val) {
-  const hp = this.HP + val;
+
+playerSchema.methods.incrementHP = function (val) {
+  const hp = +this.HP + val;
   this.HP = Math.min(Math.max(0, hp), this.maxHP);
 };
 
-const Player = mongoose.model("Player", schema);
+playerSchema.methods.incrementExp = function (val) {
+  const addExp = +this.exp + val;
+  this.exp = addExp;
+  if (this.exp >= this.maxExp) {
+    this.exp = this.exp - this.maxExp;
+    this.maxExp += 10;
+    this.level++;
+  }
+};
+
+const Player = mongoose.model("Player", playerSchema);
 
 module.exports = {
   Player
